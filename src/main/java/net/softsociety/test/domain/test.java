bac99a4a77358acf90731ab10fe58ef22fd869e2
kyoungjin1020 <kyoungjin1020@gmail.com>
2023-07-25 15:20:29 +0900

    test data 추가

---
+++ src/main/java/net/softsociety/test/domain/test.java
@@ -0,0 +1,14 @@
+package net.softsociety.test.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class test {
+	
+	int num;
+
+}
