1f057310b550664ed9e614c0fbcf2096782355cb
sung_ori <l9707so@naver.com>
2023-07-26 14:23:03 +0900

    sung_ori VO object

--- src/main/java/net/softsociety/exam/domain/Board.java
+++ src/main/java/net/softsociety/exam/domain/Board.java
@@ -3,10 +3,19 @@ package net.softsociety.exam.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 /**
  * 게시글 정보
  */
 public class Board {
-	
+	int boardnum;
+	String memberid;
+	String title;
+	String contents;
+	String inputdate;
+	String category;
+	char soldout;
+	String byerid;
 }
--- src/main/java/net/softsociety/exam/domain/Reply.java
+++ src/main/java/net/softsociety/exam/domain/Reply.java
@@ -3,10 +3,17 @@ package net.softsociety.exam.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 /**
  * 리플 정보
  */
 public class Reply {
-
+	
+	int replynum;
+	int boardnum;
+	String memberid;
+	String replytext;
+	String inputdate;
 }
