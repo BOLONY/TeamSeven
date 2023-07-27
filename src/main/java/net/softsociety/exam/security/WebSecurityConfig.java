6d644eb0949df4008d605cb7d35c27904156b80b
sung_ori <l9707so@naver.com>
2023-07-27 14:32:39 +0900

    글 읽기 시큐리티 해제

--- src/main/java/net/softsociety/exam/security/WebSecurityConfig.java
+++ src/main/java/net/softsociety/exam/security/WebSecurityConfig.java
@@ -26,6 +26,7 @@ public class WebSecurityConfig {
         .authorizeRequests()
         .antMatchers(
         		"/",
+        		"/board/readForm",
                 "/image/**",
                 "/css/**",
                 "/board/soldHome",
